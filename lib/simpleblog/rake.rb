b419318aa9914c2a502289af5239d67cbd3bc91d
Caroline Salib <carolinesalibc@gmail.com>
2022-04-21 19:06:29 -0600

    Add post files template via rake task

--- lib/simpleblog/rake.rb
+++ lib/simpleblog/rake.rb
@@ -1,5 +1,36 @@
 desc "Install simpleblog basic structure"
 task :"simpleblog:install" do
-  # Seriously, nothing
-  p "ai rob"
+  # gem_path = File.dirname(__dir__)
+  # local_path = Dir.pwd
+  # p gem_path
+  # p local_path
+
+  Dir.mkdir("app/posts") unless Dir.exist?("app/posts")
+
+  copy_template_file("app/posts/posts.yml")
+  copy_template_file("app/posts/1.md")
+  copy_template_file("app/posts/2.md")
+
+  # TODO: add controller
+  # TODO: if controller already exist ask to overrite
+
+  # TODO: add css
+  # TODO: if css already exist ask to overrite
+end
+
+def copy_template_file(path)
+  gem_path = File.dirname(__dir__)
+
+  if File.exist?(path)
+    $stdout.puts "#{path} already exist. Do you want to override? (y/n) default: y"
+    input = $stdin.gets.chomp
+
+    return if input == "n"
+
+    p "Overriden #{path}"
+  else
+    p "Created #{path}"
+  end
+
+  copy_file("#{gem_path}/simpleblog/template/#{path}", path)
 end
